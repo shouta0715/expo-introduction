9ba6fa6ff24746a274dd3e8bbd682233f47ac044
shouta0715 <Myheroes0715@gmail.com>
2024-12-10 00:28:43 +0900

    add: native wind

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { View, Pressable, Text } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
@@ -9,64 +9,28 @@ type Props = {
 export function Button({ label, theme }: Props) {
   if (theme === 'primary') {
     return (
-      <View
-        style={[
-          styles.buttonContainer,
-          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
-        ]}
-      >
+      <View className="h-16 mt-5 w-[320px]">
         <Pressable
-          style={[styles.button, { backgroundColor: '#fff' }]}
+          className="border-4 bg-white border-yellow-500 rounded-xl items-center p-4 justify-center flex-row gap-4 w-full h-full"
           onPress={() => alert('You pressed a button.')}
         >
           <FontAwesome
             name="picture-o"
             size={18}
             color="#25292e"
-            style={styles.buttonIcon}
+            className="pe-2"
           />
-          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>
-            {label}
-          </Text>
+          <Text className="text-black text-base">{label}</Text>
         </Pressable>
       </View>
     );
   }
 
   return (
-    <View style={styles.buttonContainer}>
-      <Pressable
-        style={styles.button}
-        onPress={() => alert('You pressed a button.')}
-      >
-        <Text style={styles.buttonLabel}>{label}</Text>
+    <View className="w-[320px] h-16 mt-5 flex items-center justify-center p-1">
+      <Pressable onPress={() => alert('You pressed a button.')}>
+        <Text className="text-white text-lg">{label}</Text>
       </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  buttonContainer: {
-    width: 320,
-    height: 68,
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 3,
-  },
-  button: {
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  buttonIcon: {
-    paddingRight: 8,
-  },
-  buttonLabel: {
-    color: '#fff',
-    fontSize: 16,
-  },
-});
